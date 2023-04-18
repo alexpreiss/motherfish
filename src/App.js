7823855adf3c6c446793248969d3ea25e3630e22
Alex Preiss <alexspreiss@gmail.com>
2023-04-17 19:32:14 -0700

    new favicon and overall vibe switch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import SpotifyPlayer from 'react-spotify-player'
+// import SpotifyPlayer from 'react-spotify-player'
 
-import paper from './images/paper.png'
-import guitarMan from './images/guitarman.png'
-import kojiro from './images/kojiro.png'
-import alexpreiss from './images/alexpreiss.png'
+import waterTemple from './images/water-temple.jpg'
+// import alexpreiss from './images/alexpreiss.png'
 import spotifyLogo from './images/logo-spotify.png'
 import appleMusicLogo from './images/logo-apple-music.png'
 import youtubeLogo from './images/logo-youtube.png'
 import soundcloudLogo from './images/logo-sound-cloud.png'
 import instagramLogo from './images/logo-instagram.png'
 import tiktokLogo from './images/logo-tiktok.png'
-import dcfn from './images/dcfn1.png'
 
 function link (href, imgSrc, text) {
   return (
@@ -28,7 +25,7 @@ function link (href, imgSrc, text) {
         padding: '20px 0px',
         justifyContent: 'space-around',
         textDecoration: 'none',
-        // backgroundColor: 'rgba(235, 64, 52, 0)',
+        backgroundColor: 'white',
         // boxShadow: '0px 1px 1px 0px black',
         borderRadius: '10px'
       }}
@@ -91,7 +88,7 @@ function App () {
           height: '100vh'
         }}
         alt="paper"
-        src={paper}
+        src={waterTemple}
       />
 
       <div
@@ -127,21 +124,7 @@ function App () {
             maxWidth: '800px'
           }}
         >
-          <img
-            style={{ width: '20%', objectFit: 'scale-down' }}
-            alt="drawing of face"
-            src={kojiro}
-          />
-          <img
-            style={{ width: '50%', objectFit: 'scale-down' }}
-            alt="alex preiss"
-            src={alexpreiss}
-          />
-          <img
-            style={{ width: '25%', objectFit: 'scale-down' }}
-            alt="guitar player"
-            src={guitarMan}
-          />
+          <div style={{ color: 'white' }}>alex preiss</div>
         </div>
 
         <div
@@ -152,31 +135,33 @@ function App () {
             marginTop: '30px'
           }}
         >
-          <div style={{ width: '80%', maxWidth: '800px' }}>
-            <SpotifyPlayer
-              size={{ height: '80px', width: '100%' }}
-              uri="spotify:track:1dHbJMZhZiN3tUfHg8NRjR"
-            />
+          <div
+            style={{
+              width: '80%',
+              maxWidth: '800px',
+              display: 'flex',
+              justifyContent: 'center'
+            }}
+          >
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/1P0QO-1OHcg"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
           </div>
         </div>
 
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            marginTop: '30px'
-          }}
-        >
-          <div style={{ width: '80%', maxWidth: '800px' }}>
-            <SpotifyPlayer
-              size={{ height: '80px', width: '100%' }}
-              uri="spotify:track:6TEk7dTCl7wqQRVBSkKhik"
-            />
-          </div>
-        </div>
+        {link(
+          'https://www.youtube.com/channel/UCKx3YtPJvUL9EZI2FC95q5w',
+          youtubeLogo,
+          'YouTube'
+        )}
 
-        {link('https://ffm.to/dcfn', dcfn, 'D,C,FN pre-save')}
+        {link('https://soundcloud.com/alexfuck', soundcloudLogo, 'Soundcloud')}
 
         {link(
           'https://open.spotify.com/artist/2LSPkV9a9Gsk4xMWSkLUov?si=0rwgXikEQMqYccULI45CSg',
@@ -185,17 +170,15 @@ function App () {
         )}
 
         {link(
+          'https://open.spotify.com/artist/5orDqEWdsiG6KcltddRwRq',
+          spotifyLogo,
+          'Spotify (shyman)'
+        )}
+
+        {link(
           'https://music.apple.com/us/artist/alex-preiss/1555415354',
           appleMusicLogo,
           'Apple Music'
-        )}
-
-        {link('https://www.youtube.com/c/alexfuck', youtubeLogo, 'YouTube')}
-
-        {link(
-          'https://soundcloud.com/alexspreiss',
-          soundcloudLogo,
-          'Soundcloud'
         )}
 
         {link('https://www.tiktok.com/@alex.preiss/', tiktokLogo, 'TikTok')}
